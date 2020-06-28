24ee50f9c2f56ed6ba0716461a296ca855e73256
KANGDAHYE <46888650+KANGDAHYE@users.noreply.github.com>
2020-06-28 21:56:23 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -24,7 +24,21 @@ navbarMeun.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  // console.log(event.target.dataset.link);
+  //자주 사용될 scrollIntoView 함수로 만듦
+  // const scrollTo = document.querySelector(link);
+  // scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+//Handle click on "contact me" button home
+
+const homeContactBtn = document.querySelector(".home__contact");
+homeContactBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
